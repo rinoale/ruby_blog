c4ad0d936f2f50287f82883a86d2d1f7fdeff4ae
rinoale <rinoale@hanmail.net>
2016-05-23 12:41:08 +0000

    add pixelkit and top controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'top#index'
   get 'articles/new'
 
   get 'welcome/index'
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
 	resources :comments
   end
   
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
