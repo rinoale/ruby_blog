b6891c6ef53c4bc10de5ab33a517dfcee035eb2c
rinoale <rinoale@hanmail.net>
2016-06-25 05:24:31 +0000

    Style/AlignHash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "rinoale", password: "4478",
-except: [:index, :show]
+                               except: [:index, :show]
 
   def index
     @articles=Article.all
