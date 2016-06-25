9d39569bcb234a2af446db67fd3424613962e0a2
rinoale <rinoale@hanmail.net>
2016-06-25 05:26:52 +0000

    Style/EmptyLinesAroundBlockBody

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160617103202) do
-
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
@@ -36,5 +35,4 @@ ActiveRecord::Schema.define(version: 20160617103202) do
     t.string   "emailid"
     t.string   "password"
   end
-
 end
